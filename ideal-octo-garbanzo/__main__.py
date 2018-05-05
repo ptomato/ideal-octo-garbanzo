2a10b67f9ddd5bced8f6e6a25be05cddce23c59d
Philip Chimento <philip.chimento@gmail.com>
2018-05-05 15:46:53 -0700

    Comment on issue opened

--- ideal-octo-garbanzo/__main__.py
+++ ideal-octo-garbanzo/__main__.py
@@ -1,14 +1,43 @@
+import aiohttp
 import os
 from aiohttp import web
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
+
+router = routing.Router()
+
+
+@router.register('issues', action='opened')
+async def issue_opened_event(event, gh, *args, **kwargs):
+    """Whenever an issue is opened, greet the author and say thanks."""
+    url = event.data['issue']['comments_url']
+    author = event.data['issue']['user']['login']
+    message = f"""Thanks for the report @{author}! :tada: A maintainer will be
+    along soon to take a look.
+
+    :donut: We appreciate your taking the time to file a bug report. :donut:
+
+    —Sincerely, ideal-:octocat:-garbanzo the Bot"""
+    await gh.post(url, data={'body': message})
 
 
 async def main(request):
-    return web.Response(status=200, text='Hello world!')
+    body = await request.read()
+    secret = os.environ.get('GH_SECRET')
+    token = os.environ.get('GH_AUTH')
+
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, 'ptomato', oauth_token=token)
+        await router.dispatch(event, gh)
+
+    return web.Response(status=200)
 
 
 if __name__ == '__main__':
     app = web.Application()
-    app.router.add_get('/', main)
+    app.router.add_post('/', main)
     port = os.environ.get('PORT')
     if port is not None:
         port = int(port)
